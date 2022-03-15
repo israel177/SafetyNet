c4105e3098dd3fcd7e57af5505a3cab097ea364e
Israel Aboukrat <israel@bycol.ai>
2022-03-15 08:57:25 +0100

    First commit
    init projet

---
+++ src/test/java/com/app/SafetyNet/SafetyNetApplicationTests.java
@@ -0,0 +1,16 @@
+package com.app.SafetyNet;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.*;
+
+
+@SpringBootTest
+class SafetyNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
